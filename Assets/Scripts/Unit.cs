20f47e72b13f3f9fb4dd81d7e93e52f5f00e94ea
Szukács Marcell <spiglebach@gmail.com>
2021-10-08 10:41:13 +0200

    add own unit storage

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -1,3 +1,4 @@
+using System;
 using Mirror;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,11 +7,28 @@ public class Unit : NetworkBehaviour {
     [SerializeField] private UnityEvent onSelected;
     [SerializeField] private UnityEvent onDeselected;
 
+    public static event Action<Unit> ServerOnUnitSpawned;
+    public static event Action<Unit> ServerOnUnitDespawned;
+    public static event Action<Unit> AuthorityOnUnitSpawned;
+    public static event Action<Unit> AuthorityOnUnitDespawned;
+
     private UnitMovement unitMovement;
 
     private void Awake() {
         unitMovement = GetComponent<UnitMovement>();
     }
+
+    #region Server
+
+    public override void OnStartServer() {
+        ServerOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopServer() {
+        ServerOnUnitDespawned?.Invoke(this);
+    }
+
+    #endregion
 
     #region Client
 
@@ -24,6 +42,16 @@ public class Unit : NetworkBehaviour {
     public void Deselect() {
         if (!hasAuthority) return;
         onDeselected?.Invoke();
+    }
+
+    public override void OnStartClient() {
+        if (!isClientOnly || !hasAuthority) return;
+        AuthorityOnUnitSpawned?.Invoke(this);
+    }
+
+    public override void OnStopClient() {
+        if (!isClientOnly || !hasAuthority) return;
+        AuthorityOnUnitDespawned?.Invoke(this);
     }
 
     #endregion
