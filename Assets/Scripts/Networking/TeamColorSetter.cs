721dc82108a0c1c5a97c4fb5d2d2939fc317140b
Szukács Marcell <spiglebach@gmail.com>
2021-10-16 20:22:57 +0200

    add minimap

--- Assets/Scripts/Networking/TeamColorSetter.cs
+++ Assets/Scripts/Networking/TeamColorSetter.cs
@@ -1,8 +1,10 @@
 using Mirror;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TeamColorSetter : NetworkBehaviour {
     [SerializeField] private Renderer[] colorRenderers = new Renderer[0];
+    [SerializeField] private Image[] imagesToColor = new Image[0];
     [SyncVar(hook = nameof(HandleTeamColorUpdated))] private Color teamColor = new Color();
 
     #region Server
@@ -19,6 +21,9 @@ public class TeamColorSetter : NetworkBehaviour {
     private void HandleTeamColorUpdated(Color oldColor, Color newColor) {
         foreach (var renderer in colorRenderers) {
             renderer.material.color = newColor;
+        }
+        foreach (var image in imagesToColor) {
+            image.color = newColor;
         }
     }
 
